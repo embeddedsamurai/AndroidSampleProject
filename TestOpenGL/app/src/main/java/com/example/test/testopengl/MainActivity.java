f01adeaa2a3c233474f2894038c847399a4433f4
shusaku SONE <embedded.samurai@gmail.com>
2016-05-10 11:37:52 +0900

    add toast and logcat

--- TestOpenGL/app/src/main/java/com/example/test/testopengl/MainActivity.java
+++ TestOpenGL/app/src/main/java/com/example/test/testopengl/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.test.testopengl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     MyGLView myGLView;
 
     @Override
@@ -13,18 +16,30 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
         myGLView = new MyGLView(this);
         setContentView(myGLView);
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+        String string;
+        string ="onCreate()";
+        Log.d(TAG, "debug: " + string);
     }
 
     @Override
     protected void onResume(){
         super.onResume();
         myGLView.onResume();
+        Toast.makeText(this, "onResume()", Toast.LENGTH_LONG).show();
+        String string;
+        string ="onResume()";
+        Log.d(TAG, "debug: " + string);
     }
 
     @Override
     protected void onPause(){
         super.onPause();
         myGLView.onPause();
+        Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
+        String string;
+        string ="onPause()";
+        Log.d(TAG, "debug: " + string);
     }
 
 
