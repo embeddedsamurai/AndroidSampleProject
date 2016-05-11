e834f8999287eee0c4d1edd1bec8bd2438e4f04d
shusaku SONE <embedded.samurai@gmail.com>
2016-05-11 13:19:12 +0900

    add MyCube.java

--- TestOpenGL/app/src/main/java/com/example/test/testopengl/MyRenderer.java
+++ TestOpenGL/app/src/main/java/com/example/test/testopengl/MyRenderer.java
@@ -1,6 +1,7 @@
 package com.example.test.testopengl;
 
 import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -10,14 +11,31 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class MyRenderer implements GLSurfaceView.Renderer{
 
-    @Override
-    public void onDrawFrame(GL10 gl){
+    MyCube cube = new MyCube();
 
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config){
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glDepthFunc(GL10.GL_LEQUAL);
     }
 
     @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height){}
+    public void onSurfaceChanged(GL10 gl, int width, int height){
+        gl.glViewport(0,0,width,height);
+
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        GLU.gluPerspective(gl, 45f, (float)width/height, 1f, 50f);
+    }
 
     @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config){}
+    public void onDrawFrame(GL10 gl){
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BITS);
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0,0,-3f);
+
+        cube.draw(gl);
+    }
+
 }
